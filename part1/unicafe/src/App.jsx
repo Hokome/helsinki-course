4d9d1a9fa3ca03f760a8c29d4f3e081766f9b8f1
Hokome <marwan.esseghir@hva.nl>
2023-10-19 13:45:57 +0200

    Unicafe step 3 - 5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,24 +11,22 @@ const Handler = (value, setter) => {
   return () => setter(value + 1);
 }
 
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
+  if (all == 0) {
+    return (
+      <div>
+        <h1>Statistics 📊</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   const average = (good - bad) / all;
   const positive = good / all;
   const positiveText = (positive * 100).toString().concat('%');
 
   return (
     <div>
-      <h1>Give feedback ⭐</h1>
-      <FeedbackButton text="Good 🙂" handler={Handler(good, setGood)}></FeedbackButton>
-      <FeedbackButton text="Neutral 😐" handler={Handler(neutral, setNeutral)}></FeedbackButton>
-      <FeedbackButton text="Bad 🙁" handler={Handler(bad, setBad)}></FeedbackButton>
       <h1>Statistics 📊</h1>
       <FeedbackStat label="Good" value={good}></FeedbackStat>
       <FeedbackStat label="Neutral" value={neutral}></FeedbackStat>
@@ -36,6 +34,23 @@ const App = () => {
       <FeedbackStat label="All" value={all}></FeedbackStat>
       <FeedbackStat label="Average" value={average}></FeedbackStat>
       <FeedbackStat label="Positive" value={positiveText}></FeedbackStat>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback ⭐</h1>
+      <FeedbackButton text="Good 🙂" handler={Handler(good, setGood)}></FeedbackButton>
+      <FeedbackButton text="Neutral 😐" handler={Handler(neutral, setNeutral)}></FeedbackButton>
+      <FeedbackButton text="Bad 🙁" handler={Handler(bad, setBad)}></FeedbackButton>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }
