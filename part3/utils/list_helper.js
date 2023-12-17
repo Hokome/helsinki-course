5d211f4339edb363e26dfd827040cc91c19fadb8
Hokome <xrhokom@gmail.com>
2023-12-17 11:29:58 +0100

    Favorite blog test

--- part3/utils/list_helper.js
+++ part3/utils/list_helper.js
@@ -4,7 +4,7 @@ const dummy = (blogs) => {
   return 1;
 };
 const totalLikes = (blogs) => {
-  if (blogs == 0) return 0;
+  if (blogs.length === 0) return 0;
 
   const result = blogs.reduce((sum, blog) => {
     return sum + blog.likes;
@@ -12,7 +12,26 @@ const totalLikes = (blogs) => {
   return result;
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  let maxIndex = 0;
+  for (let i = 0; i < blogs.length; i++) {
+    const element = blogs[i];
+    if (blogs[maxIndex].likes < element.likes)
+      maxIndex = i;
+  }
+
+  const max = blogs[maxIndex];
+  return {
+    title: max.title,
+    author: max.author,
+    likes: max.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
